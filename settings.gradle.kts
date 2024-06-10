05f2386124fa7c52a1c1e9ba5e5014e3af9aca22
Ruchir Kumbhare <ruchir00kumbhare00@gmail.com>
2024-06-10 23:23:33 +0530

    refactor project structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
+rootProject.name = "splitwise"
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
-include("src:abc")
-findProject(":src:abc")?.name = "abc"
+include("core:splitwise")
+findProject(":core:splitwise")?.name = "splitwise"
